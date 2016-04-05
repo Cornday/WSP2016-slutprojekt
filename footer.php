4fa2aa68ea74d22d436174e3380e7a2b305bb587
Cornday <svinsnok4@gmail.com>
2016-04-05 13:22:36 +0200

    ANother thry

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jakob
+ * Date: 2016-04-08
+ * Time: 12:20
+ */
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jakob
+ * Date: 2016-04-05
+ * Time: 12:51
+ */
