f373617241f7bd4e94d3328e8b892980011eab92
Cornday <svinsnok4@gmail.com>
2016-04-12 13:26:55 +0200

    Merge remote-tracking branch 'origin/master'

--- header.php
+++ header.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html>
 <html lang="">
 <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.css">
+
     <meta charset="UTF-8">
     <title>FiGy Book</title>
 
 </head>
 <body>
 
+<ul class="breadcrumb">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Library</a></li>
+    <li class="active">Data</li>
+</ul>
 
+<div class="container well">
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Heading 1</h1></div>
+            <div class="col-md-6">
+                <h2>Heading 2</h2>
+            </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <h3>Heading 3</h3>
+            <h4>Heading 4</h4>
+            <h5>Heading 5</h5></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <h6>Heading 6</h6>
+            <p class="lead">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p></div>
+    </div>
+    </div>
+</div>
